a4391ace354e6eb2ec9a2129770a8f9848ed10c3
halkman <halk-man@mail.ru>
2021-12-24 12:06:47 +0500

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -30,8 +30,8 @@ namespace FirstApp
             trafficLight.Agge = 34;
 
             Office office = new Office();
-            
-            
+
+
             if (office.Otdel.Type == "ГИП" && office.Comp.Ar == "Power")
             {
                 Console.WriteLine("В отделе {0} на {1} этаже ", office?.Otdel?.Rang ?? "Главный архитектор", office?.Otdel?.Etag);
@@ -44,45 +44,45 @@ namespace FirstApp
         }
     }
 
-  /*  static Department GetCurrentDepartment()
+    /*  static Department GetCurrentDepartment()
 
-    {
+      {
 
-        Department department = new Department();
+          Department department = new Department();
 
 
-        *//*if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
-        {
-            Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
-        }*//*
-        else
-        {
-            Console.WriteLine("Fuck you");
-        }
-        return department;
-    }
+          *//*if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
+          {
+              Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+          }*//*
+          else
+          {
+              Console.WriteLine("Fuck you");
+          }
+          return department;
+      }
 
-    class Bus
-	{
-		public int? Value;
+      class Bus
+      {
+          public int? Value;
 
-		public void PrintStatus()
-		{
-			if (Value.HasValue)
-			{
-				Console.WriteLine("В автобусе {0] пассажиров", Value);
-			}
-			else
-			{
-				Console.WriteLine("Автобус пуст");
-			}
-		}
+          public void PrintStatus()
+          {
+              if (Value.HasValue)
+              {
+                  Console.WriteLine("В автобусе {0] пассажиров", Value);
+              }
+              else
+              {
+                  Console.WriteLine("Автобус пуст");
+              }
+          }
 
-	}
-*/
+      }
+  */
     class Otdel
     {
-        public string Type ;
+        public string Type;
         public string Rang;
         public int Etag;
     }
@@ -95,7 +95,7 @@ namespace FirstApp
 
     class Comp
     {
-        public string Ar ;
+        public string Ar;
         public string Sm;
     }
 
@@ -108,17 +108,107 @@ namespace FirstApp
             get { return agge; }
             set { agge = value; }
         }
-       private void ChangeColor(string color)
+        private void ChangeColor(string color)
         {
 
         }
-       
-        public string  GetColor()
+
+        public string GetColor()
         {
-            
+
         }
     }
-    
+
+    /// <summary>
+    /// ---------------------- Задание 6.6.2 -------------------
+    /// </summary>
+    class User
+    {
+        private string login;
+        private string Login
+        {
+            get
+            {
+                return login;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    Console.WriteLine("Вы ввели недостаточную длину логина ");
+                }
+                else
+                {
+                    login = value;
+                }
+            }
+        }
+        private string post;
+        public string Post
+        {
+            get
+            {
+                return post;
+            }
+            set
+            {
+
+                if (!value.Contains('@'))
+                {
+                    Console.WriteLine("Вы не ввели символ @ ");
+                }
+                else
+                {
+                    post = value;
+                }
+            }
+        }
+
+
+
+    }
+
+     class Triangle
+    {
+        private int Aa;
+        private int Bb;
+        private int Cc;
+
+        public int aa
+        {
+            get { return Aa; }
+            set 
+            {
+                if (value > 0 && Bb + Cc > value)
+                {
+                    Aa = value;
+                }
+               
+            }
+        }
+        public int bb
+        {
+            get { return Bb; }
+            set
+            {
+                if (value > 0 && Aa + Bb > value)
+                {
+                    Bb = value;
+                }
+            }
+        }
+        public int cc 
+        {
+            get { return Cc; }
+            set
+            {
+                if ((value > 0) && Aa + Bb > value)
+                {
+                    Cc = value;
+                }
+            }
+        }
+    } 
 }
 
 
