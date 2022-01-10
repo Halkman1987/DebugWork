5162f909b777a83058e3c8536fa590625691394f
halkman <halk-man@mail.ru>
2022-01-10 15:29:21 +0500

    8

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,33 @@ namespace FirstApp
         }
     }
 
-} 
+    
+    public class Drive
+    {
+        public string Name { get; }
+        public long TotalSpace { get; }
+        public long FreeSpace { get; }
+
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+        
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+        public void CreateFolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
+
+    }
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+    }
+    
+}
 
 
