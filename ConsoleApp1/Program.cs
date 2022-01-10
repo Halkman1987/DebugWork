f489df22b13070ff6c92d55914075b2c1186a7d5
halkman <halk-man@mail.ru>
2022-01-10 23:26:49 +0500

    8.2.4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,10 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
+            DelPath();
+           // CreatePath();
+            ViewCatalog();
+            GetCatalogs();
             // получим системные диски
             DriveInfo[] drives = DriveInfo.GetDrives();
             // Пробежимся по дискам и выведем их свойства
@@ -22,9 +26,72 @@ namespace FirstApp
                 }
             }
         }
-    }
 
+        static void GetCatalogs()
+        {
+            string dirName = @"C:\\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+
+                foreach (string d in dirs) // Выведем их все
+                    Console.WriteLine(d);
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+
+                foreach (string s in files)   // Выведем их все
+                    Console.WriteLine(s);
+            }
+        }
+        static void ViewCatalog()
+        {
+            try
+            {
+                DirectoryInfo viewpath = new DirectoryInfo(@"C:\\");
+                if (viewpath.Exists)
+                {
+                    Console.WriteLine(viewpath.GetDirectories().Length);
+                    Console.WriteLine(viewpath.GetFiles().Length);
+                }
+                DirectoryInfo newdirect = new DirectoryInfo(@"c:\54321");
+                if (!newdirect.Exists)
+                newdirect.Create();
+                Console.WriteLine(viewpath.GetDirectories().Length);
+                Console.WriteLine(viewpath.GetFiles().Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        static void DelPath()
+        {
+            try
+            {
+                DirectoryInfo delpth = new DirectoryInfo(@"C:\User\sourse\");
+                delpth.Delete(true);
+                Console.WriteLine("Path delete");
+            }
+             catch(Exception ex) { Console.WriteLine(ex.Message); }
+
+        }
+        static void CreatePath()
+        {
+            DirectoryInfo crpth = new DirectoryInfo(@"c:\User\sourse\");
+            if (!crpth.Exists)
+            {
+                crpth.Create();
+                crpth.CreateSubdirectory("1234567");
+                Console.WriteLine(crpth.GetDirectories().Length);
+            }
+        }
+    }
     
+
     public class Drive
     {
         public string Name { get; }
