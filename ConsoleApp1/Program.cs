7d624e65f0fbf470803abfbf45706f434bf8e845
halkman <halk-man@mail.ru>
2022-01-13 00:22:12 +0500

    8.6.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,80 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace FirstApp
 {
     
-    
-    class Programm
+    class Program
     {
         static void Main(string[] args)
         {
             
+            string dirname = @"C:\User";
+            if (Directory.Exists(dirname))
+            {
+                Console.WriteLine($"Папка доступна {dirname} "); 
+            }
+            else
+            {
+                Directory.CreateDirectory(dirname);
+            }
+            
+            FolderExicute.Checkfolder(dirname);
+
+        }
+        public static class FolderExicute
+        {
+            public static void Checkfolder(string pathname)
+            {
+                if (Directory.Exists(pathname))
+                {
+                    CheckFiles(pathname);
+                    string [] dirpath = Directory.GetDirectories(pathname);
+                    foreach (string dir in dirpath)
+                    {
+                        Checkfolder(dir);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Кончились папки");
+                }
+            }
+            public static void CheckFiles(string pathname)
+            {
+                if (!Directory.Exists(pathname))
+                {
+                    string[] namepath = Directory.GetFiles(pathname);
+                    if (namepath.Length != 0)
+                        foreach (string v in namepath)
+                        {
+                            TimeSpan interval = TimeSpan.FromMinutes(30);
+                            var dt = File.GetLastWriteTime(v);
+                            DateTime time = DateTime.Now;
+                            try
+                            {
+                                if ((time - dt) > interval)
+                                {
+                                    Console.WriteLine("Время вышло");
+                                    File.Delete(v);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Время не пришло ");
+                                }
+                            }
+                            catch(Exception e)
+                            { Console.WriteLine(e.Message); }
+                        }
+                }
+                
+            } 
         }
 
     }
   
 }
-
 
 
 
