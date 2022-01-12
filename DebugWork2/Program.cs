deee78f9a32ad33fd428daa0dc516557028bb92e
halkman <halk-man@mail.ru>
2022-01-12 15:12:36 +0500

    1

--- DebugWork2/Program.cs
+++ DebugWork2/Program.cs
@@ -64,4 +64,43 @@ namespace Serialization
             Console.ReadLine();
         }
     }
+
+    public class Drive
+    {
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+
+        public string Name { get; }
+        public long TotalSpace { get; }
+        public long FreeSpace { get; }
+
+
+
+
+
+        
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+        
+        public void Createfolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
+        
+        //-----------------------------------------------------------
+        public Folder folder { get; } // Создание папки напрямую 
+        public void createfiles(string name) 
+        {
+            folder.Files.Add(name);
+        }
+    }
+
+
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+    }
 }
