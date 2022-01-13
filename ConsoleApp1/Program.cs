6c1990e492c43ce2c532ea8acf4c152c4116fea5
halkman <halk-man@mail.ru>
2022-01-13 15:14:29 +0500

    drive

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.IO;
-
+using System.Linq;
 namespace FirstApp
 {
     class Program
     {
-
         static void Main(string[] args)
         {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives.Where(d :DriveInfo => d.DriveFormat == DriveType.Fixed))
+            {
+                Console.WriteLine(drive.Name);
+                Console.WriteLine(drive.DriveType);
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"{drive.TotalSize}");
+                    Console.WriteLine($"{drive.TotalFreeSpace}");
+                    Console.WriteLine($"{drive.VolumeLabel}");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
 
+            }
 
 
 
