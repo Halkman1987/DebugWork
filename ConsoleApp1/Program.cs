ef17ccae2373a84d975595400c94066d79af3440
halkman <halk-man@mail.ru>
2022-01-17 15:47:04 +0500

    7.7

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,77 +3,173 @@ using System.Collections.Generic;
 
 class Program
 {
+    class Mypickpoints
+    {
+        public string adresses;
 
+       public Mypickpoints(string adresses)
+        {
+            this.adresses = adresses;
+        }
+    }
     public static void Main(string[] args)
     {
-        User user = new User();
-        Console.WriteLine("Введите ваше имя : ");
-        user.Name = Console.ReadLine();
-        Console.WriteLine("Введите вашу фамилию : ");
-        user.Family = Console.ReadLine();
-        Console.WriteLine("Введите ваш номер телефона :");
-        user.Telephone = Console.ReadLine();
-
-        Console.WriteLine("Выберите товар и укажите количесвто :");
-        string tov = Console.ReadLine();
-        switch (tov)
+        Mypickpoints[] mypickpoints = new Mypickpoints[2];
+        mypickpoints[0] = new Mypickpoints("Какойто адресс 1");
+        mypickpoints[1] = new Mypickpoints("Какойто адресс 2");
+        /* List<string> Mypickpoints = new List<string>();
+        Mypickpoints.Add("какойто адресс 1");
+        Mypickpoints.Add("какойто адресс 2");*/
+        Korziva korz = new Korziva();
+        //Заводим номенклатуру товаров 
+        Product[] tov = new Product[11];
+        tov[0] = new Product (1 , "Гриф Олимпийский с замками 20кг",  20000 );
+        tov[1] = new Product(2 ,"Диск 20кг",  3000);
+        tov[2] = new Product(3 ,"Диск 10кг",  2000);
+        tov[3] = new Product(4 ,"Диск 5кг",  1000);
+        tov[4] = new Product(5 ,"Гиря 32кг",  2500);
+        tov[5] = new Product(6 ,"Гиря 24кг",  2000);
+        tov[6] = new Product(7 ,"Гиря 16кг",  1500);
+        tov[7] = new Product(8 ,"Гантель 5кг", 600);
+        tov[8] = new Product(9 ,"Гантель 8кг", 700);
+        tov[9] = new Product(10 ,"Гантель 12кг", 800);
+        tov[10] = new Product(11 ,"Гантель 16кг", 900);
+        //Добавление в Список товаров из класса Продукт
+        //
+        List<Product> list = new List<Product>();
+        list.Add(tov[0]);
+        list.Add(tov[1]);
+        list.Add(tov[2]);
+        list.Add(tov[3]);
+        list.Add(tov[4]);
+        list.Add(tov[5]);
+        list.Add(tov[6]);
+        list.Add(tov[7]);
+        list.Add(tov[8]);
+        list.Add(tov[9]);
+        list.Add(tov[10]);
+        foreach(var ls in list)// Вывод товаров из массива в консоль
         {
-            case 1:
-                Console.WriteLine("ganteli");
-                break;
-            case 2:
-                Console.WriteLine();
-                break;
-            case 3:
-
-
+            Console.WriteLine($" {ls.pd} -- {ls.name} = {ls.cash} ");
         }
+        
+        Console.WriteLine("\tДобро пожаловать в магазин спорт-инвентаря :");
+        Console.WriteLine("\tИмеются данные товары :");
+        foreach(var p in tov)
+        {
+            
+            Console.WriteLine( $" {p.pd} - {p.name} = {p.cash} рублей");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Выберите товары, введите номер товара :");
+        int numtov = Convert.ToInt32(Console.ReadLine());
+        Product vibor = tov[numtov];
+        Console.WriteLine(" Укажите количество :");
+        int muchtov = Convert.ToInt32(Console.ReadLine());
+        /* switch (numtov)
+         {
+             case 1:
+                 korz.NameTov = vibor;
 
 
-        List<string> tovar = new List<string>();
-
-
-        Order<Delivery> order = new Order<Delivery>();
-
-        Product product = new Gantel();
-
-
+         }*/
+        Adress adress = new Adress();//инициализировал элемент класса Адрес 
+        Console.ReadLine();
+        Order<Delivery> order1 = new Order<Delivery>();
         Console.WriteLine("Выберите способ доставки:\n 1 - Если нужна доставка на дом \n 2 - Если требуется доставка до Постамата \n 3 - Если вы хотите забрать товар из магазина");
         int change = Convert.ToInt32(Console.ReadLine());
         switch (change)
         {
             case 1:
-                order.Delivery = new HomeDelivery();
-
-                Console.WriteLine("Вы выбрали доставку на дом, введите свой адрес для доставки :");
-
-                Adress adress = new Adress();
-
-                Console.WriteLine("Введите индекс вашего населеннго пункта :");
-                adress.indexcity = Console.ReadLine();
-                Console.WriteLine("Введите ваш город :");
-                adress.city = Console.ReadLine();
-                Console.WriteLine("Введите вашу улицу :");
-                adress.street = Console.ReadLine();
-                Console.WriteLine("Введите номер дома :");
-                adress.house = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Введите номер квартиры:");
-                adress.appartment = Convert.ToInt32(Console.ReadLine());
+                order1.Delivery = new HomeDelivery();
+                HomeDelivery homeDelivery = new HomeDelivery();
+                homeDelivery.SetHomeAdr();
+                homeDelivery.Getadress();
                 break;
             case 2:
                 Console.WriteLine("Вы выбрали доставку в постамат");
-
-                Console.WriteLine(PickPointDelivery.boxnumber);
+                Console.WriteLine("Доступны следующие точки:");
+                foreach (var adr in adress.PickAdr)
+                    Console.WriteLine(adr);
+                Order<Delivery> order2 = new Order<Delivery>();
+                order2.Delivery = new PickPointDelivery(32442);
+                Console.WriteLine(order1.Delivery);
                 break;
             case 3:
                 Console.WriteLine("Вы выбрали доставку в магазин");
-                Console.WriteLine(ShopDelivery.shopadress);
+                Order<Delivery> order3 = new Order<Delivery>();
+                order3.Delivery = new ShopDelivery();
 
-                new Order<HomeDelivery>= new(,)
+               // new Order<HomeDelivery>= new(,)
 
 
-                    break;
+                break;
         }
+
+        #region Code_switch
+        /* object tov = Console.ReadLine();
+         switch (tov)
+         {
+             case 1:
+                 Console.WriteLine("ganteli");
+                 break;
+             case 2:
+                 Console.WriteLine();
+                 break;
+             case 3:
+
+
+
+         } */
+        #endregion
+        Console.WriteLine("Выведем введенный адрес");
+
+        adress.InputAdressPick();
+        adress.InputAdressHome();
+
+        Order<Delivery> order = new Order<Delivery>();
+        
+        order.Delivery.Getadress();
+        order.DisplayAddress();
+        // Adress adresss = new Adress();
+
+        //-----------------  от Олега  ----  Вариант Выбора Доставки  ----------------------------------------
+        Console.WriteLine("Введите  choice :");
+        int choice = Convert.ToInt32(Console.ReadLine());
+         switch (choice)
+         {
+             case 1:
+                 {
+                     order.Delivery = new ShopDelivery();
+                    
+                     break;
+                 }
+             case 2:
+                 {
+                    int key = 0;
+                     Console.WriteLine("Доступны следующие точки:");
+                     foreach (var adr in adress.PickAdr)
+                     Console.WriteLine(adr);
+
+                     Console.WriteLine("Выберете по номеру (от 1 до {0})", adress.PickAdr.Length);
+                    //  key = key.GetNumber(1, adress.PickAdr.Length);
+                    //order.Delivery = new PickPointDelivery(adress.PickAdr[key - 1]);
+                     break;
+                 }
+             case 3:
+                 {
+                     order.Delivery = new HomeDelivery(/*buyer.Address*/);//buyer - покупатель и запускаем метод адресс
+                     break;
+                 }
+             default:
+                 {
+                     order.Delivery = new ShopDelivery();
+                     break;
+                 }
+         }
+
+        //--------------------------------------------------------------------------------------------------
 
 
         /*Product P1 = new Product(1, "Футболка с длинным рукавом", 1518.2, Product.Type.Футболка, "Вьетнам", Product.Sex.Men, "XXL");
@@ -90,59 +186,116 @@ class Program
 
     abstract class Delivery
     {
-        public Adress adress;
-        public abstract string getInfo();
+        abstract public Adress Adress { get; set; } 
+        abstract public string PriceDelivery { get; set; }
+
+        abstract public void Getadress();
+        
     }
 
     class HomeDelivery : Delivery
     {
-
-        public string viphome;
-
-        public override string getInfo()
+        public override Adress Adress { get; set;}
+        public override string PriceDelivery { get; set;}
+       
+        public void SetHomeAdr()
         {
-            return $"{adress}-{viphome}";
+            
+            Console.WriteLine("Вы выбрали доставку на дом, введите свой адрес для доставки :");
+
+            Console.WriteLine("Введите индекс вашего населеннго пункта :");
+            Adress.indexcity = Console.ReadLine();
+            Console.WriteLine("Введите ваш город :");
+            Adress.city = Console.ReadLine();
+            Console.WriteLine("Введите вашу улицу :");
+            Adress.street = Console.ReadLine();
+            Console.WriteLine("Введите номер дома :");
+            Adress.house = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите номер квартиры:");
+            Adress.appartment = Convert.ToInt32(Console.ReadLine());
+           
         }
-
-        public void SetHome()
+        public override void Getadress() // Метод вывода адреса 
         {
-
+            Console.WriteLine($"DELIVERY выводим из Метод Getadress адресс из Delivery Город {Adress.city}, улица {Adress.street}, дом {Adress.house}");
         }
     }
 
     class PickPointDelivery : Delivery
     {
-        public int boxnumber = 349035;
+        public int boxnumber;
+        public override Adress Adress { get; set; }
+        public override string PriceDelivery { get; set; }
+        public void SetPickAdr()
+        {
+
+            Console.WriteLine("Вы выбрали доставку в Постамат :");
+            Console.WriteLine($"{Adress.PickAdr[0]} или {Adress.PickAdr[1]}");
+            Console.WriteLine($" номер вашего заказа :{boxnumber}");
+            
+        }
+        public PickPointDelivery(int boxnumber)
+        {
+            this.boxnumber = boxnumber;
+            
+        }
+    public override void Getadress()
+    {
+        Console.WriteLine("DELIVERY выводим из Метод Getadress адресс из Delivery");
     }
+}
 
     class ShopDelivery : Delivery
     {
+        public override Adress Adress { get; set; }
+        public override string PriceDelivery { get; set; }
+
         public string shopadress = "г.Уфа, ул.Черниковская 87 ";
+        public override void Getadress()
+        {
+            Console.WriteLine("DELIVERY выводим из Метод Getadress адресс из Delivery");
+        }
     }
 
     // Заказ 
     class Order<TDelivery> where TDelivery : Delivery
     {
-        public TDelivery Delivery;
+        public TDelivery Delivery;//что это нам дает
 
-        public int Number;
+        public int Number;//номер ордера
 
         public string Description;
+        public string TovaryVkorzine;// список выбранных товаров
 
-        public Product Product;
+        public Product[] product;//что это нам дает
+        public string prod;
+        public void InProduct(Product[] product)
+        {
+            product = product;
+            foreach(Product p in product)
+            {
+                Console.WriteLine(p.pd.ToString(), p.name, "-", p.cash, "рублей");
+            }
+        }
+        public Order()
+        {
+            
+        }
 
         public Order(TDelivery delivery, int number, string description, Product product)
         {
             Delivery = delivery;
             Number = number;
             Description = description;
-            Product = product;
+           // this.product = product;
         }
 
         public void DisplayAddress()
         {
-            Console.WriteLine(Delivery.getInfo());
-            Product.name;
+            Console.WriteLine("ORDER выводим из Метод Дисплей адресс из ордера");
+            Console.WriteLine(Delivery.Adress);//откуда берется адресс 
+            Console.WriteLine("выводим Product.name");
+            //Console.WriteLine(product);
         }
         //private Product product;
         //public Order (Product product)
@@ -152,61 +305,33 @@ class Program
 
         // ... Другие поля
     }
-    abstract class Product
+     class Product //Заполнение из Main
     {
-        public int much;
-        public string snaryad;
-        public abstract string name { get; }
-        public virtual void SetTovar(int num)
+        public int pd;
+        public string name;
+        public int cash;
+        public Product(int id,string Name, int cash)
         {
-            Console.WriteLine("Укажите количество товаров :");
-
-        }
-        public Product(int much)
-        {
-            this.much = much;
+            pd = id;
+            name = Name;
+            this.cash = cash;
         }
     }
-    class Gantel : Product
+    class Gantel 
     {
-
         public string gantel;
-
-        public Gantel() : base(much)
-        {
-
-        }
-
-        public override string name => "gantel";
-
-        public override void SetTovar(int NumTov)
-        {
-
-        }
     }
-    class Girya : Product
+    class Girya 
     {
-        public string girya = "Гиря";
-        public override void SetTovar(int NumTov)
-        {
-
-        }
+       
     }
-    class Shanga : Product
+    class Griff 
     {
-        public string shtanga = "Штанга";
-        public override void SetTovar(int NumTov)
-        {
-
-        }
+        public string shtanga ;
     }
-    class Blini : Product
+    class Blini 
     {
-        public string blin = "Блин";
-        public override void SetTovar(int NumTov)
-        {
-
-        }
+        public string blin;
     }
 
 
@@ -223,6 +348,7 @@ class Program
         public int house { get { return House; } set { House = value; } }
         public int appartment { get { return Appartment; } set { Appartment = value; } }
 
+        
         public Adress(string index, string city, string street, int house, int appart)
         {
             indexcity = index;
@@ -231,19 +357,59 @@ class Program
             this.house = house;
             appartment = appart;
         }
-
+        public void InputAdressPick()
+        {
+            Console.WriteLine($"{PickAdr[0]} ,\n {PickAdr[1]} ");
+            Console.WriteLine($"{IndexCity} , {City} , {Street}, {House}" );
+        }
+        public void InputAdressHome()
+        {
+            Console.WriteLine($"{IndexCity} , {City} , {Street}, {House}");
+        }
         public Adress()
         {
+
         }
+        public string ShopAdr;
+        public string[] PickAdr;
+        public Adress(string pick)
+        {
+            PickAdr[0] = "Первая точка какойто город1 улица1 и дом1";
+            PickAdr[1] = "Вторая точка какойто город2 улица2 и дом2";
+        }
+        public void GetNumber()
+        {
+
+        }
+
     }
 
     public class User
     {
+        public Adress adress;
+
         public string Name;
         public string Family;
         public string Telephone;
+        
+        public User( string name, string family, string telephone)
+        {
+            Name = name;
+            Family = family;
+            Telephone = telephone;
+        }
+       /* User user = new User();
+        Console.WriteLine("Введите ваше имя : ");
+        user.Name = Console.ReadLine();
+        Console.WriteLine("Введите вашу фамилию : ");
+        user.Family = Console.ReadLine();
+        Console.WriteLine("Введите ваш номер телефона :");
+        user.Telephone = Console.ReadLine();*/
     }
-
+    class Korziva
+    {
+        public string NameTov;
+    }
 
 }
 
--- DebugWork2/Program.cs
+++ DebugWork2/Program.cs
@@ -7,262 +7,32 @@ namespace FinalTask
 {
     class Program
     {
-        [Serializable]
-        public class Student
-        {
-            public string Name { get; set; }
-            public string Group { get; set; }
-            public DateTime DateOfBirth { get; set; }
-            public Student(string name, string group, DateTime dat)
-            {
-                Name = name;
-                Group = group;
-                DateOfBirth = dat;
-            }
-        }
-       
 
         static void Main()
         {
-            List <string> group = new List<string>();
-            Student[] student = new Student[3];
-            student[0] = new Student("Dima", "one", new DateTime(1987,03,02));
-            student[1] = new Student("Igor", "two", new DateTime(1988, 04, 03));
-            student[2] = new Student("вася", "three", new DateTime(1989, 05, 03));
-            string Stud = @"C:\Users\User\Desktop\MyStudentsForm.dat";
-            // Создали папку 
-            string filePath = @"C:\Users\User\Desktop\Student";
-            DirectoryInfo dirInfo = new DirectoryInfo(filePath);
-            if (!dirInfo.Exists)
-                dirInfo.Create();
-
-            try
-            {
-                //создаем форматтер 
-                BinaryFormatter formatter = new BinaryFormatter();
-                //открываем поток 
-                using (FileStream fs = new FileStream(@"C:\Users\User\Desktop\MyStudentsForm.dat", FileMode.OpenOrCreate))
-                {
-                   // сериализация файла 
-                    formatter.Serialize(fs, student);
-                    Console.WriteLine("File Serialized ");
-                    Console.WriteLine("--------------------------------------------------------");
-                }
-            }
-            catch (Exception ex) { Console.WriteLine(ex.Message); }
-
-            
-            //---------Рабочий Вариант Десериализации и чтения из файла в консоль -----------
-            if (File.Exists(Stud))
-            {
-                try
-                {
-                    BinaryFormatter bf = new BinaryFormatter();
-                    using (FileStream fs = new FileStream(Stud, FileMode.Open,FileAccess.Read))
-                    {
-                        List<string> Grouplist = new List<string>();
-
-                        Student[] nstudent = (Student[])bf.Deserialize(fs);
-                        Console.WriteLine("Прошла Десериализация");
-                        Console.WriteLine("--------------------------------------------------------");
-                        Console.WriteLine(" Считали  :");
-                        foreach (Student stud in nstudent)
-                        {
-                           
-                            //получили данные 
-                            string NewPath = Path.Combine(filePath, stud.Group + ".txt");//имя файла
-                            if (!File.Exists(NewPath))
-                            {
-                                Console.WriteLine($"Создадим файл {NewPath}");
-                            }
-                            else if (!Grouplist.Contains(stud.Group))
-                            {
-                                File.Delete(NewPath);   
-                            }
-                            FileInfo fileInfo = new FileInfo(NewPath);
-                            try
-                            {
-                                using(StreamWriter fss = fileInfo.AppendText())
-                                {
-                                    fss.Write(stud.Name + "___");
-                                    fss.Write(stud.DateOfBirth.ToString("D"));
-                                }
-                            }
-                            catch (Exception e)
-                            {
-                                Console.WriteLine(e.Message);
-                            }
-                            if (!Grouplist.Contains(stud.Group))
-                            {
-                               Grouplist.Add(stud.Name);
-                            }
-                           
-                        }
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-            else
-            {
-                Console.WriteLine($"{Stud} - Ненайден");
-            }
-
            
+
         }
-        
+
 
     }
-}
-
-/*//Внутри раскидать всех студентов из файла по группам (каждая группа-отдельный текстовый файл),
-//в файле группы студенты перечислены построчно в формате "Имя, дата рождения".
-Console.WriteLine("Создаем массив Групп  :");
-Groups[] gp = new Groups[student.Length];
-
-foreach (Student stud in student)
-{
-    for (int i = 0; i < student.Length; i++)
+    class MotherBoard
     {
-        gp[i] = new Groups(stud.Name, stud.DateOfBirth.ToString("D"));
-
+        public string Name ; 
     }
-}
-
-using (StreamWriter sw = File.CreateText(@"C:\Users\User\Desktop\Student\MyStudentsForm.txt"))
-{
-    Console.WriteLine(stud.Name);
-    Console.WriteLine(stud.Group);
-    Console.WriteLine(stud.DateOfBirth.ToString("D"));
-    *//* Console.WriteLine(stud.Name);
-     Console.WriteLine(stud.Group);
-     Console.WriteLine(stud.DateOfBirth.ToString("D"));
-     sw.WriteLine(stud.Name);
-     sw.WriteLine(stud.Group);
-     sw.WriteLine();*//*
-} 
-*/
-
-
-
-/* //ReadValues(Stud);
-           //ReadBin(SetFileNew);
-           // Пишем
-           string Stud1 = @"C:\Users\User\Desktop\Students1.dat";
-           WriteValues(Stud1);
-           // Считываем*/
-
-/* static void WriteValues(string Stud1)
- {
-     // Создаем объект BinaryWriter и указываем, куда будет направлен поток данных
-     //string stud1 = @"C:\Users\User\Desktop\BinaryFile.bin";
-     if (File.Exists(Stud1))
-     {
-         // FileStream fs = new FileStream()
-         using (BinaryWriter writer = new BinaryWriter(File.Open(Stud1, FileMode.Append)))
-         {
-             // записываем данные в разном формате
-             // writer.Write();
-
-         }
-     }
-
- }
-
-
- public static void ReadValues(string Stud)
- {
-
-     string Name;
-     string Group;
-     string DateOfbirth;
-     Student student = new Student(Name, Group, DateTime);
-     if (File.Exists(Stud))
-     {
-         using (FileStream fs = new FileStream(Stud, FileMode.Open))
-         // using (var stream = File.Open(Stud, FileMode.Open))//, FileAccess.Read))
-         {
-             var newstu =
-                  using (BinaryReader reader = new BinaryReader(fs, Encoding.Default))
-             // using (BinaryReader reader = new BinaryReader(stream, Encoding.UTF8 ))
-             {
-                 Name = reader.ReadString();
-                 Group = reader.ReadString();
-                 DateOfbirth = reader.ReadString();
-             }
-             Console.WriteLine("Из файла считано:");
-             Console.WriteLine($"Строка:{Name} , {Group}, {DateOfbirth} ");
-         }
-
-     }
-
- }
-*/
-/* static void ReadBin(string SetFileNew)
-{
-
- //string SetFileNew = @"C:\Users\User\Desktop\BinaryFile.bin";
- if (File.Exists(SetFileNew))
- {
-     string Name;
-     string Group;
-     string DateOfbirth;
-
-     using (BinaryReader reader = new BinaryReader(File.Open(SetFileNew, FileMode.Open)))
-     {
-         Name = reader.ReadString();
-         Group = reader.ReadString();
-         DateOfbirth = reader.ReadString();
-     }
-     Console.WriteLine(Name);
- }
-
-
-}
-}*/
-
-//---------------------- Рабочий вариант Записи Бинарника--------------------------
-//создаем врайтер и записываем им данные студента в файл
-/*
-using (BinaryWriter bw = new BinaryWriter(File.Open(Stud, FileMode.OpenOrCreate)))
-                {
-                    foreach (Student st in student)
-                    {
-                        
-                        bw.Write(st.Name);
-                        bw.Write(st.Group);
-                        bw.Write(st.DateOfBirth.ToString("D"));
-                    }
-                }
-
-//---------Рабочий Вариант Десериализации и чтения из файла в консоль -----------
-if (File.Exists(Stud))
-{
-    try
+    class SystemUnit
     {
-        BinaryFormatter bf = new BinaryFormatter();
-        using (FileStream reader = new FileStream(Stud, FileMode.Open, FileAccess.Read))
+        private MotherBoard motherBoard;
+
+        public SystemUnit(MotherBoard motherBoard)
         {
-            Student[] nstudent = (Student[])bf.Deserialize(reader);
-            Console.WriteLine(" Считали  :");
-            foreach (Student stud in nstudent)
-            {
-                Console.WriteLine(stud.Name);
-                Console.WriteLine(stud.Group);
-                Console.WriteLine(stud.DateOfBirth.ToString("D"));
-            }
+            this.motherBoard = motherBoard;
+        }
+        public SystemUnit()
+        {
+            motherBoard = new MotherBoard();
         }
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-    }
+
 }
-else
-{
-    Console.WriteLine($"{Stud} - Ненайден");
-}
-//-------------------------------------------------------------------------------------*/
+
