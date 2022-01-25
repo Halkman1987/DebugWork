aec34f081138d35b934ce3c8e01fe1f58a3e6fa0
halkman <halk-man@mail.ru>
2022-01-25 22:15:49 +0500

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,71 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
-
-class Program
+namespace DebugWork1
 {
 
 
-    static void Main()
+    class Program
     {
-        Calc Calc = new Calc();
-        Operate operation = Calc.Nothing;
-        char c = default;
-        char[] signs = new char[] { '+', '-', '/', '*', 'в' };
-        Console.WriteLine("Добро пожаловать в программу калькулятор!");
-        Console.Write("Введите число: ");
-        decimal a = 0;
-        decimal b = 0;
-        a = a.GetN(decimal.MinValue, decimal.MaxValue);
-        bool check = true;
-        decimal? result;
-        do
+        static ILogger logger { get;  set; }
+        public static void Main(string[] args)
         {
-            Console.Write("Введите число: ");
-            b = b.GetN(decimal.MinValue, decimal.MaxValue);
-            Console.Write("Введите операцию (для выхода из программы - 'в'): ");
-            c = c.GetC(signs);
-            switch (c)
-            {
-                case '+': { operation = Calc.Add; break; }
-                case '-': { operation = Calc.Subtract; break; }
-                case '/': { operation = Calc.Divide; break; }
-                case '*': { operation = Calc.Multiply; break; }
-                case 'в': { check = false; continue; }
-            }
+           logger = new Logger();
+           CalculateP calcp = new CalculateP(logger);
+           CalculateM calcm = new CalculateM(logger);
+           
             try
             {
-                operation -= Calc.Nothing;
-                result = operation?.Invoke(a, b);
-                Console.WriteLine("{0} {1} {2} = {3}", a, c, b, result);
-                a = result ?? a;
+                switch ()
+                {
+                    case 
+                }
+                Console.WriteLine("Введите первое число : ");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите второе число : ");
+                double y = Convert.ToDouble(Console.ReadLine());
+                var resultp = calcp.Calc(x, y);
+                var resultm = calcm.Calc(x, y);
+                Console.WriteLine();
+                Console.WriteLine($"Результатом сложения является чиcло : {resultp}");
+                Console.WriteLine($"Результатом вычитания является чиcло : {resultm}");
+              
             }
-            catch (DivideByZeroException)
+            catch (FormatException ex)
             {
-                Console.WriteLine("Что-ж вы, батенька, на ноль то делите?!");
+                logger.Error(ex.Message);
+                Console.WriteLine(ex.Message);
             }
-            catch (Exception ex)
-            { Console.WriteLine(ex.ToString()); }
+            finally
+            {
+                Console.WriteLine(" Вы ввели корректные значения , тем самым не вызвал исключения формата данных! ");
+            }
         }
-        while (check);
-        Console.ReadKey();
-    }
 
-    public delegate decimal Operate(decimal a, decimal b);
-    public interface ICalc
-    {
-        decimal Add(decimal a, decimal b);
-        decimal Multiply(decimal a, decimal b);
-        decimal Subtract(decimal a, decimal b);
-        decimal Divide(decimal a, decimal b);
     }
-    class Calc : ICalc
+    public interface ILogger
     {
-        public decimal Add(decimal a, decimal b) { return a + b; }
-        public decimal Multiply(decimal a, decimal b) { return a * b; }
-        public decimal Subtract(decimal a, decimal b) { return a - b; }
-        public decimal Divide(decimal a, decimal b) { return a / b; }
-        public decimal Nothing(decimal a, decimal b) { return 0; }
+        void Event(string message);
+        void Error(string message);
+    }
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+    public interface ICalc<T,Z>
+    {
+        public double Calc(T x, T y);
     }
 }
-
-
