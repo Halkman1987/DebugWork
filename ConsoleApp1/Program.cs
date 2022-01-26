a95fbdbb09e7ad2a713202e5b8ab42051852deec
halkman <halk-man@mail.ru>
2022-01-26 16:48:40 +0500

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,51 +6,44 @@ namespace DebugWork1
 
     class Program
     {
-        static ILogger logger { get;  set; }
+        static ILogger logger { get; set; }
         public static void Main(string[] args)
         {
-           logger = new Logger();
-            var calcp = DebugWork1.CalculateP(logger);
-            var calcm =
+            logger = new Logger();
+            CalculateP calcp = new CalculateP(logger);
+            CalculateM calcm = new CalculateM(logger);
+            Console.WriteLine("Добро пожаловать в примитивный калькулятор!");
+            string choice = null;
             try
             {
-                Console.WriteLine("Введите первое число : ");
-                double x = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Введите второе число : ");
-                double y = Convert.ToDouble(Console.ReadLine());
-                var resultp = calc.Calc(x, y);
-                var resultm = calc2.Calc(x, y);
-                Console.BackgroundColor = ConsoleColor.Blue;
-                Console.WriteLine();
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"Результатом сложения является чиcло : {resultp}");
-
-                Console.ResetColor();
-                //Console.BackgroundColor = ConsoleColor.Red;
-
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Результатом вычитания является чиcло : {resultm}");
-                Console.ResetColor();
-
-
+                do
+                {
+                    Console.WriteLine("Введите первое число : ");
+                    double x = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введите второе число : ");
+                    double y = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Выберите операцию + или - :");
+                    choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "+":
+                            double resp = calcp.Calc(x, y);
+                            Console.WriteLine($"Результатом сложения является чиcло : {resp}");
+                            break;
+                        case "-":
+                            double resm = calcm.Calc(x, y);
+                            Console.WriteLine($"Результатом вычитания является чиcло : {resm}");
+                            break;
+                        default:
+                            Console.WriteLine("Давайте еще раз введем цифры и всётаки выберем операцию + или - !!!");
+                            break;
+                    }
+                }
+                while (choice != "+" && choice != "-" );
             }
-            catch (FormatException ex)
-            {
-                Console.WriteLine(" Вы ввели некорректные значения ");
-                Console.WriteLine(ex.Message);
-
-            }
-            finally
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.WriteLine(" Вы ввели корректные значения , тем самым не вызвал исключения формата данных! ");
-                Console.ResetColor();
-            }
-
-
+            catch (FormatException ex) {logger.Error(ex.Message);}
+            catch (Exception e) { logger.Error(e.Message);}
         }
-
     }
     public interface ILogger
     {
@@ -61,15 +54,19 @@ namespace DebugWork1
     {
         public void Error(string message)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(message);
+            Console.ResetColor();
         }
 
         public void Event(string message)
         {
-            Console.WriteLine(message); 
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
-    public interface ICalc<T,Z>
+    public interface ICalc<T, Z>
     {
         public double Calc(T x, T y);
     }
